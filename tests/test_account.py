033b1cee87758654a078be56b8d54d4ab5672aaa
Arsen Vartanov <arsen10012004@gmail.com>
2026-09-25 21:10:16 +0300

    Оборачиваю тесты в test_account.py в класс TestAccount

--- tests/test_account.py
+++ tests/test_account.py
@@ -5,11 +5,12 @@ from locators import ACCOUNT_BUTTON, LOGOUT_BUTTON
 from settings import WAIT_SECONDS
 
 
-def test_navigate_to_account(driver, logged_in_user):
-    driver.find_element(*ACCOUNT_BUTTON).click()
+class TestAccount:
+    def test_navigate_to_account(self, driver, logged_in_user):
+        driver.find_element(*ACCOUNT_BUTTON).click()
 
-    WebDriverWait(driver, WAIT_SECONDS).until(
-        expected_conditions.visibility_of_element_located(LOGOUT_BUTTON)
-    )
+        WebDriverWait(driver, WAIT_SECONDS).until(
+            expected_conditions.visibility_of_element_located(LOGOUT_BUTTON)
+        )
 
-    assert "/account" in driver.current_url
+        assert "/account" in driver.current_url
